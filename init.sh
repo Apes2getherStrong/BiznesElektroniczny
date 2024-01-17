5d7b78c83ea48b8992be8ebe87eb6deec8859e17
Oskar <118617644+Drillllll@users.noreply.github.com>
2024-01-17 21:32:37 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -7,4 +7,14 @@ MYSQL_DATABASE="BE_188889"
 SQL_DUMP_FILE="dump.sql"
 mysql -h"$MYSQL_HOST" -P"$MYSQL_PORT" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
 mysql -h"$MYSQL_HOST" -P"$MYSQL_PORT" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE" < "$SQL_DUMP_FILE"
+
+if [ -d "install" ]; then
+    rm -rf install
+fi
+
+if [ -d "config" ]; then
+    rm -rf config
+fi
+
+
 exec apache2-foreground
